0cc397154be0d9eaadf792812c5420f8a939c41d
Sergei MIkhailov <sergei.mikhailov@gmail.com>
2023-03-26 16:01:43 +0200

    read configuration parameters from os env

--- bot/config.py
+++ bot/config.py
@@ -7,8 +7,8 @@ from pathlib import Path
 config_dir = Path(__file__).parent.parent.resolve() / "config"
 
 # load yaml config
-with open(config_dir / "config.yml", 'r') as f:
-    config_yaml = yaml.safe_load(f)
+# with open(config_dir / "config.yml", 'r') as f:
+#     config_yaml = yaml.safe_load(f)
 
 # load .env config
 config_env = dotenv.dotenv_values(config_dir / "config.env")
@@ -21,12 +21,12 @@ config_env = dotenv.dotenv_values(config_dir / "config.env")
 # new_dialog_timeout = config_yaml["new_dialog_timeout"]
 # enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 # mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
-telegram_token = os.getenv('TELEGRAM_TOKEN', config_yaml.get("telegram_token"))
-openai_api_key =  os.getenv('OPENAI_API_KEY', config_yaml.get("openai_api_key"))
-use_chatgpt_api =  os.getenv('USE_CHATGPT_API', config_yaml.get("use_chatgpt_api", True))
-allowed_telegram_usernames =  config_yaml.get('allowed_telegram_usernames', os.getenv("TELEGRAM_USERNAMES", "").split(','))
-new_dialog_timeout =  int(os.getenv("NEW_DIALOG_TIMEOUT", config_yaml.get("new_dialog_timeout")))
-enable_message_streaming = os.getenv('ENABLE_MESSAGE_STREAMING', config_yaml.get("enable_message_streaming", True))
+telegram_token = os.getenv('TELEGRAM_TOKEN')
+openai_api_key =  os.getenv('OPENAI_API_KEY')
+use_chatgpt_api =  os.getenv('USE_CHATGPT_API', True)
+allowed_telegram_usernames =  os.getenv("TELEGRAM_USERNAMES", "").split(',')
+new_dialog_timeout =  int(os.getenv("NEW_DIALOG_TIMEOUT", 300))
+enable_message_streaming = os.getenv('ENABLE_MESSAGE_STREAMING', True)
 mongodb_uri = "mongodb://mongo:"+config_env.get('MONGODB_PORT', "27017")
 
 # chat_modes
